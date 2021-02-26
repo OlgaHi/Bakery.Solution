4ea4362733c1447500d3e097e3496b19a592aa64
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-26 11:19:09 -0800

    add user interface logic to collect bread quantity

--- Bakery.Solution/Bakery/Models/Program.cs
+++ Bakery.Solution/Bakery/Models/Program.cs
@@ -1,4 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+
+namespace Bakery.Models 
+{
+  public class Program 
+  {
+    public static void Main() 
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine("Today we have a special offer: Buy 2, get 1 free. A single loaf costs $5");
+      string input = Console.ReadLine();
+      int breadQuantity = int.Parse(input);
+      
+    }
+  }
+}
 
