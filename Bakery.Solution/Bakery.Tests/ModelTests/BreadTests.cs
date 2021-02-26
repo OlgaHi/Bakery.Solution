a62b5ffd01c70fb9c5d6391586be899f3f6e1cfb
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-26 09:28:27 -0800

    add test bread constructor, passed

---
+++ Bakery.Solution/Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests 
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
