55435b0c12f5ecf05902448ed4526292e4981830
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-26 10:10:56 -0800

    add logic to constructor

--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -1,3 +1,16 @@
 using System;
 using System.Collections.Generic;
 
+namespace Bakery.Models 
+{
+  public class Bread 
+  {
+   public int Quantity { get; }
+
+   public Bread(int quantity)
+    {
+      Quantity = quantity;
+    }
+
+  }
+}
